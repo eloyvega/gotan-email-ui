0f7d0693292904c5d0c6cb264189d1f44639a87e
Eloy Vega <eloy.vega@gmail.com>
2015-02-13 10:57:50 -0600

    Use of _id instead of username

--- client/templates/dashboard/inbox.js
+++ client/templates/dashboard/inbox.js
@@ -1,8 +1,5 @@
 Template.inbox.helpers({
     mails: function () {
         return Inbox.find({recipient:this._id},{sort: {arrive_date: -1}});
-    },
-    username: function(){
-        return this._id;
     }
 });
