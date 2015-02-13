54c1eb530e26d60a749dcd114ea0a03c9b11c299
Eloy Vega <eloy.vega@gmail.com>
2015-02-13 10:48:47 -0600

    Reverse order in mails

--- client/templates/dashboard/inbox.js
+++ client/templates/dashboard/inbox.js
@@ -1,6 +1,6 @@
 Template.inbox.helpers({
     mails: function () {
-        return Inbox.find({recipient:this._id});
+        return Inbox.find({recipient:this._id},{sort: {arrive_date: -1}});
     },
     username: function(){
         return this._id;
