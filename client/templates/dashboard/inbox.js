9b0c0296780bd2a7534eeaa8266adab567bec1bb
Eloy Vega <eloy.vega@gmail.com>
2015-02-13 10:43:31 -0600

    Remove some lines

--- client/templates/dashboard/inbox.js
+++ client/templates/dashboard/inbox.js
@@ -1,10 +1,6 @@
 Template.inbox.helpers({
     mails: function () {
-        console.log(this._id);
         return Inbox.find({recipient:this._id});
-    },
-    domain: function(){
-        return domain_name;
     },
     username: function(){
         return this._id;
